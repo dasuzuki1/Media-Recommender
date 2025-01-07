6b7f61f0d29565e86b2080460e7884748357c0cb
dasuzuki1 <danielsuzuki6@gmail.com>
2025-01-07 14:01:02 -0500

    Changed SQL Schema to make relation with user and Series in list.

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -21,3 +21,16 @@ CREATE TABLE Users (
     username VARCHAR(100),
     avatar_url TEXT
 );
+
+
+CREATE TABLE UserAnime (
+    user_anime_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER REFERENCES Users(user_id) ON DELETE CASCADE, -- Relate to Users
+    anime_id INTEGER REFERENCES Anime(anime_id),
+    rating FLOAT,
+    favorite BOOLEAN,  
+    status VARCHAR(20),
+    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE(user_id, anime_id)  -- Add the UNIQUE constraint
+);
+
